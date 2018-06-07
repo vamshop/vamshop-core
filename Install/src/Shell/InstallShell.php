c8f0370dffee06743313b34123fc21b9a10f31b0
vam <vamshop@gmail.com>
2018-06-07 18:54:15 +0300

    Корректировка кода

--- Install/src/Shell/InstallShell.php
+++ Install/src/Shell/InstallShell.php
@@ -213,6 +213,6 @@ class InstallShell extends Shell
         }
 
         $this->out();
-        $this->success('Congratulations, Croogo has been installed successfully.');
+        $this->success('Congratulations, VamShop has been installed successfully.');
     }
 }
