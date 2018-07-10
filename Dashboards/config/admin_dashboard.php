34e5a4a148ca7746fc4d602a6ac52bba38e4db72
root <vamshop@gmail.com>
2018-07-10 16:21:03 +0300

    Корректировка названия блока

--- Dashboards/config/admin_dashboard.php
+++ Dashboards/config/admin_dashboard.php
@@ -4,7 +4,7 @@ use Croogo\Dashboards\CroogoDashboard;
 
 $config = [
     'dashboards.blogfeed' => [
-        'title' => __d('croogo', 'Croogo News'),
+        'title' => __d('croogo', 'VamShop News'),
         'cell' => 'Croogo/Dashboards.BlogFeed::dashboard',
         'column' => CroogoDashboard::RIGHT,
         'access' => ['superadmin'],
