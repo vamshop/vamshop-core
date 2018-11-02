74cca6aa535c3e3960d3e470d1fbeb5e09ab9e5b
vam <vamshop@gmail.com>
2018-11-02 22:25:01 +0300

    Корректировка API В модуле Nodes, добавлены недостающие actions

--- Nodes/src/Controller/Api/V10/NodesController.php
+++ Nodes/src/Controller/Api/V10/NodesController.php
@@ -15,6 +15,31 @@ class NodesController extends AppController
         return $this->Crud->execute();
     }
 
+    public function add()
+    {
+        return $this->Crud->execute();
+    }
+
+    public function edit()
+    {
+        return $this->Crud->execute();
+    }
+
+    public function index()
+    {
+        return $this->Crud->execute();
+    }
+    
+    public function delete()
+    {
+        return $this->Crud->execute();
+    }
+    
+    public function view()
+    {
+        return $this->Crud->execute();
+    }
+
     public function getName()
     {
     }
